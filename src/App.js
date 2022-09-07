d94e5c073e54804e07f9625b8828bd57237c44df
Oluwibe Faith <faitholuwibe@gmail.com>
2022-09-07 16:32:33 +0100

    modified version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar/Navbar";
+import HeroSection from "./components/HeroSection/HeroSection";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Product from "./components/Product/Product";
+import Products from "./components/Product/Products";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route exact path="/home" element={<HeroSection />} />
+        <Route exact path="/products" element={<Products />} />
+        <Route exact  path="/products/:id" element={<Product/>} />
+        <Route exact path="/cart" element={<Cart/>}/>
+        <Route exact path="/checkout" element={<Checkout/>} />
+      </Routes>
+      <Footer/>
     </div>
   );
 }
